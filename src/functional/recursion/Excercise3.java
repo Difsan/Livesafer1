77f47f1593236543ba28d5f0ad85a3937f83ae70
Difsan <difsanchezru@gmail.com>
2023-01-26 01:10:53 -0500

    FindVowel was fixed
    longestStri method was added to complete the FindVowel method

--- src/functional/recursion/Excercise3.java
+++ src/functional/recursion/Excercise3.java
@@ -3,12 +3,13 @@ package functional.recursion;
 public class Excercise3 {
     public static void main(String[] args){
         FindVowel ne = new FindVowel();
-        System.out.println(ne.calculate("ae"));
-        System.out.println(ne.calculate("p"));
+        System.out.println(ne.calculate("ae pop phjue ppppppp"));
+        System.out.println(ne.calculate("banano"));
+        /*System.out.println(ne.calculate("p"));
         System.out.println(ne.calculate("banano"));
         System.out.println(ne.calculate("Armando"));
         System.out.println(ne.calculate(""));
-        System.out.println(ne.calculate("aeiouAEIOU"));
+        System.out.println(ne.calculate("aeiouAEIOU"));*/
 
     }
 }
--- src/functional/recursion/FindVowel.java
+++ src/functional/recursion/FindVowel.java
@@ -1,12 +1,27 @@
 package functional.recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class FindVowel {
 
     public int calculate(String x){
+
         if (x.length() == 0){
             return 0;
         }else {
-            System.out.println(x.substring(1));
+            if (x.contains(" ")) {
+                String[] list1 = x.split(" ");
+                // you can't use .add and .remove if do this
+                //List<String> l = Arrays.asList(list1);
+                //Solution
+                List<String> l = new LinkedList<String>(Arrays.asList(list1));
+                System.out.println("The longest word in the string: " +
+                        this.longestStri(l));
+            }
+            x = x.replace(" ", "");
             int count = calculate(x.substring(1));
             if (this.isVowel(x.charAt(0))){
                 return 1+count;
@@ -19,5 +34,17 @@ public class FindVowel {
         String vowels = "aeiouAEIOU";
         boolean vp = vowels.indexOf(c)!=-1;
         return vp;
+    }
+
+    public String longestStri (List<String> y){
+
+        if (y.size()==1) return y.get(0);
+        else {
+            String present= y.get(0);
+            y.remove(0);
+            String longest = longestStri(y);
+            if (present.length()<longest.length()) return longest;
+            else return present;
+        }
     }
 }
